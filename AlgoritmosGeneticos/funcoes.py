9f94a4bc2dbf457733754a5b543c4b324b898d80
guilhermeilum <guilherme220044@ilum.cnpem.br>
2023-03-09 17:07:38 -0300

    exercicio 2 finalizado

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,11 @@
- 
+# irei utilizar uma lista para representar meu indivíduo
+# a quantidade de elementos de uma lista é a quantidade de genes.
+# cada gene varia apenas de 0 ou 1.
+
+def função_objetivo_cb(indivíduo):
+    """Computa a função objetivo para um indivíduo
+    Args:
+        Indivíduo: lista contendo todos os genes das caixas binarias.
+    Return:
+        Um valor representando a soma dos genes do indivíduo."""
+    return sum(indivíduo)
