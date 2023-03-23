340d7d4272eb9a8935f15f8846de6da6ca846f5e
guilhermeilum <guilherme220044@ilum.cnpem.br>
2023-03-23 16:53:21 -0300

    Experimento 4

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -105,3 +105,71 @@ def func_objetivo_população(população):
     Lista de valores representando os fitness dos individuos."""
     fitnnes = [função_objetivo_cb(individuo) for individuo in população]
     return fitnnes
+
+def gene_cnb(n):
+    """Gera um gene valido para um indivíduo da caixa não binaria.
+    Args:
+        n: numero maximo da caixa.
+    Return:
+        Um valor entre 0 e n."""
+    gene = randint(0,n)
+    return gene
+
+def indivíduo_cnb(valor_maximo,n):
+    """Gera um indivíduo para o problema das caixas não binarias.
+
+    Args:
+        n: número de genes do indivíduo.
+        valor_maximo: Valor maximo para a caixa binaria
+    Return:
+        Uma lista com n genes. Cada gene é um valor zero a valor_maximo.
+    """
+    indivíduo = [gene_cnb(valor_maximo) for i in range(n)]
+    return indivíduo
+
+
+def população_caixas_n_binarias(valor_maximo,tamanho, n):
+    """Cria uma populção de caixas não binarias.
+
+    arg:
+        tamanho: quantos da população irá ter na caixa binarias.
+        n: Quantidade de genes nas caixas binarias
+    return:
+        população: lista de individuos"""
+    população = [
+        indivíduo_cnb(valor_maximo,n) for _ in range(tamanho)
+    ]  # sendo n o numero de individuos.
+    return população
+
+def mutação_cnb(valor_maximo,indivíduo):
+    """Realiza a mutação de um gene no problema das caixas não binárias
+
+    Args:
+        indivíduo: uma lista representando um individuo no problema das caixas binárias.
+
+    Return:
+        Um individuo com um gene mutado.
+    """
+    gene_a_ser_mutado = randint(0, len(indivíduo) - 1)
+    indivíduo[gene_a_ser_mutado] = gene_cnb(valor_maximo)
+    return indivíduo
+
+def função_objetivo_cnb(indivíduo):
+    """Computa a função objetivo para um indivíduo
+
+    Args:
+        Indivíduo: lista contendo todos os genes das caixas binarias.
+    Return:
+        Um valor representando a soma dos genes do indivíduo."""
+    return sum(indivíduo)
+
+def func_objetivo_população_n_b(população):
+    """Calcula a função objetivo para todos os membros de uma população.
+
+    Arg:
+    '   população: lista com os individuos selecionados.
+
+    Returns:
+    Lista de valores representando os fitness dos individuos."""
+    fitnnes = [função_objetivo_cnb(individuo) for individuo in população]
+    return fitnnes
